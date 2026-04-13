06884be6a66cdc8c0d06a77c59b89e11bbf70b43
ritesh <krsingh081206@gmail.com>
2026-04-13 22:16:59 +0530

    Updated MAX FLOW to 200

--- src/models/index.js
+++ src/models/index.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(config.db.name, config.db.user, config.db.passwo
   dialect: 'postgres',
   logging: (message) => logger.debug(message),
   pool: {
-    max: 5,
+    max: 200,
     min: 0,
     acquire: 30000,
     idle: 10000,
